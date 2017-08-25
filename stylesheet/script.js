dde7b16ba8279289fd77730be5530e391134759c
Felipe Flores <fflores@nursoft.cl>
2017-08-25 05:36:57 -0300

    Add new files

--- stylesheet/script.js
+++ stylesheet/script.js
@@ -10,16 +10,5 @@ $('.dropdown-button').dropdown({
   }
 );
 
-$('.side-dropdown').dropdown({
-    inDuration: 300,
-    outDuration: 225,
-    constrainWidth: true, // Does not change width of dropdown to that of the activator
-    hover: false, // Activate on hover
-    gutter: 0, // Spacing from edge
-    belowOrigin: true, // Displays dropdown below the button
-    alignment: 'left', // Displays dropdown with edge aligned to the left of button
-    stopPropagation: false // Stops event propagation
-  }
-);
 
 $(".button-collapse").sideNav();
